feae82ce53cd28804e854ca76924b5c4ea401be2
Oleg Morozenkov <reo7sp@users.noreply.github.com>
2017-12-05 02:51:35 +0300

    Fix

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
-const extractCSS = new ExtractTextPlugin('styles.[chunkhash].css');
+const extractCSS = new ExtractTextPlugin('styles.[hash].css');
 
 module.exports = {
     entry: {
